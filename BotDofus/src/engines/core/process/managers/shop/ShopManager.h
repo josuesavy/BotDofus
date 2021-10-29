32148a4a6f3920e696586636afaf2fc9bcc7f748
Garten <josue.savy@gmail.com>
2021-10-29 17:15:34 +0200

    Shop: update protocol files, remove useless comments in ShopManager file

--- BotDofus/src/engines/core/process/managers/shop/ShopManager.h
+++ BotDofus/src/engines/core/process/managers/shop/ShopManager.h
@@ -38,36 +38,3 @@ public:
 };
 
 #endif // SHOPMANAGER_H
-
-
-//Lancer
-//[SND] > InteractiveUseRequestMessage
-//[RCV] InteractiveUsedMessage
-//[RCV] ExchangeStartedBidBuyerMessage (Filtres : Aile, Bois, Clef, etc..)
-
-//Acheter
-//[SND] > ExchangeBidHouseTypeMessage (Follow : Aile, Bois, Clef, etc..)
-//[RCV] ExchangeTypesExchangerDescriptionForUserMessage (Liste des items en vente)
-//[SND] > ExchangeBidHouseSearchMessage (Follow item dans la liste)
-//[RCV] ExchangeTypesItemsExchangerDescriptionForUserMessage (Informations sur l'item, Price : 1, 10, 100)
-//[SND] > ExchangeBidHouseBuyMessage
-//[RCV] ExchangeBidHouseBuyResultMessage
-
-
-//Vendre
-//[SND] > NpcGenericActionRequestMessage
-//[RCV] ExchangeLeaveMessage
-//[RCV] ExchangeStartedBidSellerMessage (Filtres : Aile, Bois, Clef, etc..)
-//[SND] > ExchangeBidHouseSearchMessage (Follow item dans l'inventaire)
-//[RCV] ExchangeTypesItemsExchangerDescriptionForUserMessage (Informations sur l'item, Price : 1, 10, 100)
-//[SND] > ExchangeBidHousePriceMessage (Follow item dans l'inventaire)
-//[RCV] ExchangeBidPriceForSellerMessage
-//[SND] > ExchangeObjectMovePricedMessage (Price, object's id, quantity)
-//[RCV] ExchangeBidHouseItemAddOkMessage (informations sur l'item mis en vente)
-//[RCV] ExchangeBidHouseInListUpdatedMessage (update new item put in sell)
-//[SND] > ExchangeBidHousePriceMessage (Follow item dans l'inventaire)
-//[RCV] ExchangeBidPriceForSellerMessage
-
-//Quitter
-//[SND] > LeaveDialogRequestMessage
-//[RCV] ExchangeLeaveMessage
